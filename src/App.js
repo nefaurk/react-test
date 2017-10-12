567f99d984f877cff29346771aa87c384363b4c7
Nefaur Khandker <nefaurk@gmail.com>
2017-10-12 10:56:12 -0400

    Integrating Aphrodite

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { StyleSheet, css } from "aphrodite";
+
 import Button from "./components/Button";
+
+const styles = StyleSheet.create({
+	app: {
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		padding: "16px",
+	},
+});
 
 class App extends Component {
 	render() {
 		return (
-			<div className="app">
+			<div className={css(styles.app)}>
 				<Button title="Click me!" style={Button.Style.Secondary} />
 				<Button title="No, click me!" />
 			</div>
